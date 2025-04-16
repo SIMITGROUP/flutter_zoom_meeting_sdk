db6f74e99f48a9303c91dca58ff7d3a5e9b377d2
sky <jhyong@simitgroup.com>
2025-04-16 22:18:55 +0800

    Update Podfile

--- macos/Classes/FlutterZoomMeetingSdkPlugin.swift
+++ macos/Classes/FlutterZoomMeetingSdkPlugin.swift
@@ -31,7 +31,7 @@ public class FlutterZoomMeetingSdkPlugin: NSObject, FlutterPlugin {
 
       let authContext = ZoomSDKAuthContext()
       authContext.jwtToken =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBLZXkiOiJBYlhXS2VDTFJ5Q1pyUURRWEVtMVEiLCJzZGtLZXkiOiJBYlhXS2VDTFJ5Q1pyUURRWEVtMVEiLCJtbiI6IjMyNzM1ODg2MTMiLCJyb2xlIjowLCJ0b2tlbkV4cCI6MTc0NDgxMzM2NywiaWF0IjoxNzQ0ODA5NzY3LCJleHAiOjE3NDQ4MTMzNjd9.AlbAETpb69ql83cRa9hEksAn7Ktn8Ec61N3NdY_swkI"
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBLZXkiOiJBYlhXS2VDTFJ5Q1pyUURRWEVtMVEiLCJzZGtLZXkiOiJBYlhXS2VDTFJ5Q1pyUURRWEVtMVEiLCJtbiI6IjMyNzM1ODg2MTMiLCJyb2xlIjowLCJ0b2tlbkV4cCI6MTc0NDgxNjYxNywiaWF0IjoxNzQ0ODEzMDE3LCJleHAiOjE3NDQ4MTY2MTd9.Fj3TpAQ1jSdEFS71TJzyME2pYVDCRMx8JyEFY0e932Y"
       let authResult = authService.sdkAuth(authContext)
       print("Auth result: \(authResult)")
       result("Auth result: \(authResult)")
