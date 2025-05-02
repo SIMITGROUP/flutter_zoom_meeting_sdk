778e18f1107bc3ad11bfd275d0b4da5b4d4845b6
jhyong <jhyong@simitgroup.com>
2025-05-02 12:14:05 +0800

    [Flutter] Update Example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_zoom_meeting_sdk_example/meeting_card_list.dart';
-import 'package:flutter_zoom_meeting_sdk_example/zoom_service.dart';
+import './meeting_card_list.dart';
+import './zoom_service.dart';
+import './mock/meeting_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,28 +29,6 @@ class _MyAppState extends State<MyApp> {
     _zoomService.dispose();
     super.dispose();
   }
-
-  final List<Map<String, String>> meetingData = [
-    {
-      'meetingNumber': '3273588613',
-      'password': '6SuCMB',
-      'displayName': 'John Doe',
-    },
-    {
-      'meetingNumber': '98667185101',
-      'password': 'QI8xbfyBFqt5h2lqrOYJf3e7JXNFjY.1',
-      'displayName': 'John Doe',
-      'webinarToken':
-          '3U-fmkgKTk9AdPczv7AstfKsFNHRlmbEI_3gdKSe6kE.DQcAAAAW-QXDzRYtYV9tZEFMUlJjLTFrUm82Wmo2RjlnAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-    },
-    {
-      'meetingNumber': '81905298441',
-      'password': 'PAJyyCw8jrnftFASg9IAzJ2WFSHz7Q.1',
-      'displayName': 'Yong JH',
-      'webinarToken':
-          'u9D6MKUReBvGBPIkjfVSIPHLvXHStOvtrgDHqafw0gM.DQcAAAATEe-sCRZBbmExR0taWlRrbTJ5V2J0eXQ5d2dnAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-    },
-  ];
 
   void joinMeeting(Map<String, String> meeting) {
     _zoomService.setMeetingNumber(meeting['meetingNumber'] ?? '');
